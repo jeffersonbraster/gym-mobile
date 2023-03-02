3e4f35d0cd2a91a286094564fbd8c1c57fc8c4ca
Jefferson <jeferson.oliveira@g.globo>
2023-03-02 00:03:49 -0300

    add theme and create loading

--- App.tsx
+++ App.tsx
@@ -2,16 +2,18 @@ import { Text, View, StatusBar } from 'react-native';
 import {NativeBaseProvider} from 'native-base'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import {THEME} from './src/theme'
+import { Loading } from '@components/Loading';
+
 export default function App() {
   const [fontsLoaded] = useFonts({
     Roboto_400Regular,
     Roboto_700Bold,
   })
   return (
-    <NativeBaseProvider>
+    <NativeBaseProvider theme={THEME}>
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-     
-      {fontsLoaded ? <View /> : <View />}      
+      {fontsLoaded ? <View /> : <Loading />}
     </NativeBaseProvider>
   );
 }
