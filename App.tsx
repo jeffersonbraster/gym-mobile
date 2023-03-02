048c1eeb0b04a5a5fbfb6e22e78cd767587eb528
Jefferson <jeferson.oliveira@g.globo>
2023-03-01 23:25:52 -0300

    config statusbar and assets

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
 export default function App() {
@@ -7,10 +7,10 @@ export default function App() {
     Roboto_700Bold,
   })
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024'}}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+     
       {fontsLoaded ? <Text>oie</Text> : <View />}      
     </View>
   );
 }
-
-
