1b69b0b0ecdea0d30c546fee0ee637ebfdeefd11
Jefferson <jeferson.oliveira@g.globo>
2023-03-02 23:22:45 -0300

    create component input

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,11 +1,12 @@
-import { VStack, Image, Text, Center } from "native-base";
+import { VStack, Image, Text, Center, Heading } from "native-base";
 
 import BackgroundImg from "@assets/background.png";
 import LogoSvg from "@assets/logo.svg";
+import { Input } from "@components/Input";
 
 export function Signin() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.700" px={10}>
       <Image
         source={BackgroundImg}
         alt="Um casal treinando, fazendo exercicio em uma academia."
@@ -19,6 +20,21 @@ export function Signin() {
         <Text color="gray.100" fontSize="sm">
           Treine sua mente e o seu corpo
         </Text>
+      </Center>
+
+      <Center>
+        <Heading color="gray.100" fontSize="xl" mt={6} fontFamily="heading">
+          Acesse sua conta
+        </Heading>
+
+        <Input
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          mt={4}
+        />
+
+        <Input placeholder="Senha" secureTextEntry />
       </Center>
     </VStack>
   );
