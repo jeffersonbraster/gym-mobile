de2998840135a3a95a74b70d9fb53b2c180aae6b
Jefferson <jeferson.oliveira@g.globo>
2023-03-03 23:33:10 -0300

    add icons in tab navigation

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,17 +1,62 @@
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from "@react-navigation/bottom-tabs";
 import { Exercise } from "@screens/Exercise";
 import { History } from "@screens/History";
 import { Home } from "@screens/Home";
 import { Profile } from "@screens/Profile";
 
-const { Navigator, Screen } = createBottomTabNavigator();
+import HomeSvg from "@assets/home.svg";
+import HistorySvg from "@assets/history.svg";
+import ProfileSvg from "@assets/profile.svg";
+import { useTheme } from "native-base";
+
+type AppRoutes = {
+  home: undefined;
+  history: undefined;
+  profile: undefined;
+  exercise: undefined;
+};
+
+export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
+
+const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
+  const { sizes } = useTheme();
+
+  const iconSize = sizes[6];
+
   return (
-    <Navigator>
-      <Screen name="home" component={Home} />
-      <Screen name="history" component={History} />
-      <Screen name="profile" component={Profile} />
+    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
       <Screen name="exercise" component={Exercise} />
     </Navigator>
   );
