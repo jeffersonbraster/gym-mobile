3c4e4b4b56c0da2f195474a5846e40028e6386af
Jefferson <jeferson.oliveira@g.globo>
2023-03-06 19:26:29 -0300

    create component ScreenHeader

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -31,7 +31,9 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected === item}
+            isActive={
+              groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()
+            }
             onPress={() => setGroupSelected(item)}
           />
         )}
