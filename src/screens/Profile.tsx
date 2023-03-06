61862b171295ddd1ead78ba461a39b7910fb605b
Jefferson <jeferson.oliveira@g.globo>
2023-03-06 20:01:46 -0300

    create photo in component perfil

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,9 +1,35 @@
-import { Center, Text } from "native-base";
+import { useState } from "react";
+import { Center, ScrollView, Skeleton, VStack } from "native-base";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Userphoto } from "@components/UserPhoto";
+
+const PHOTO_SIZE = 33;
 
 export function Profile() {
+  const [photoIsLoading, setPhotoIsLoading] = useState(false);
   return (
-    <Center flex={1}>
-      <Text color="white">Profile</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          {photoIsLoading ? (
+            <Skeleton
+              w={PHOTO_SIZE}
+              h={PHOTO_SIZE}
+              rounded="full"
+              startColor="gray.400"
+              endColor="gray.300"
+            />
+          ) : (
+            <Userphoto
+              source={{ uri: `https://github.com/jeffersonbraster.png` }}
+              alt="Foto do usuário"
+              size={PHOTO_SIZE}
+            />
+          )}
+        </Center>
+      </ScrollView>
+    </VStack>
   );
 }
